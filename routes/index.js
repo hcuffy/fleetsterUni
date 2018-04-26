f4cc491a55c75227075c41cf9668b8e8c1959515
Henry Cuffy <hcuffy_5@hotmail.com>
2018-04-26 05:26:14 +0200

    fix route

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,7 @@ const router = express.Router();
 router.use('/bookings', require('./bookings'));
 router.use('/bookingsLocal', require('./bookingsLocal'));
 router.use('/users', require('./users'));
-router.get('/*', (req, res) => {
-  res.send('Hello World');
-});
+
 
 
 module.exports = router;
